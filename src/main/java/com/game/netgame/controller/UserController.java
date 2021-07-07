0c3fa934394c3376a3c447b62006c9c8fbada921
mhq1065 <mhq1065@qq.com>
2021-07-07 14:26:45 +0800

    完成Controller层

--- src/main/java/com/game/netgame/controller/UserController.java
+++ src/main/java/com/game/netgame/controller/UserController.java
@@ -1,18 +1,39 @@
 package com.game.netgame.controller;
 
-import com.game.netgame.service.impl.UserServiceImpl;
-import org.mybatis.spring.annotation.MapperScan;
+import com.game.netgame.entity.User;
+import com.game.netgame.service.IUserService;
+import com.game.netgame.service.ex.InsertException;
+import com.game.netgame.service.ex.UsernameDuplicateException;
+import com.game.netgame.utils.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "user")
 public class UserController {
+    @Autowired
+    IUserService userService;
 
-
-
-    @RequestMapping(value = {"index", "/"})
-    public String index() {
-        return "hello";
+    @RequestMapping("reg")
+    public JsonResult<Void> index(User user) {
+        JsonResult<Void> result=new JsonResult<Void>();
+        try {
+            userService.reg(user);
+            result.setState(200);
+            result.setMessage("注册成功");
+        } catch (UsernameDuplicateException e) {
+            result.setState(402);
+            result.setMessage("用户名被占用");
+            e.printStackTrace();
+        }catch (InsertException e){
+            result.setState(403);
+            result.setMessage("插入数据异常");
+            e.printStackTrace();
+        }
+        return result;
     }
 
 }
